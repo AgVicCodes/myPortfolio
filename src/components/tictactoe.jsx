afbe5ebdf891805b9e0f286a61a232a9478054f2
AgVicCodes <victoragbeleye2@gmail.com>
2023-07-26 23:43:41 +0100

    Edited JSX

--- src/components/tictactoe.jsx
+++ src/components/tictactoe.jsx
@@ -4,11 +4,63 @@ import { useState } from "react";
 var myChar = "X";
 // var nextChar = "Y";
 
-function clearBoard() {
-    if (myChar) {
-        setValue(null);
+function Square() {
+    const[value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue(myChar); 
+    }
+    
+    
+
+    return <button onClick={handleClick} className="square">{value}</button>  
+    
+    
+}
+
+
+function setChar() {
+    if (myChar === 'X') {
+        myChar = 'O';
+    } else if (myChar === 'O') {
+        myChar = 'X';
     }
 }
+
+
+
+
+
+export default function Board() {
+    return <>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+
+    <button onClick={setChar} className="w-32 mt-4 mr-4 h-16 shadow-sm bg-slate-200 rounded-md">Swap Letters</button>
+    {/* <button onClick={clearBoard} className="w-32 mt-4 h-16 shadow-sm bg-slate-200 rounded-md">Clear Board</button> */}
+    </>
+}
+
+
+/* import React from "react";
+import { useState } from "react";
+
+var myChar = "X";
+// var nextChar = "Y";
+
 function Square() {
     const[value, setValue] = useState(null);
 
@@ -57,4 +109,6 @@ export default function Board() {
     <button onClick={setChar} className="w-32 mt-4 mr-4 h-16 shadow-sm bg-slate-200 rounded-md">Swap Letters</button>
     <button onClick={clearBoard} className="w-32 mt-4 h-16 shadow-sm bg-slate-200 rounded-md">Clear Board</button>
     </>
-}
+} 
+
+*/
