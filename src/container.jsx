8edcf569f56203bfdc943ff54349656cc800eff3
AgVicCodes <victoragbeleye2@gmail.com>
2023-07-27 16:59:06 +0100

    Created COntainer

--- src/container.jsx
+++ src/container.jsx
@@ -1,12 +1,83 @@
 import React from 'react'
 
+
 export default function Container() {
 	return <>
-		<div className="container cols-4 mx-auto">
-			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
-			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
-			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
-			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
+		<div className="container">
+			<img src="./logo192.png" alt="React logo" className="w-52 float-right" />
+			<img src="./logo192.png" alt="React logo" className="w-64 float-left" />
+			<div className="px-4 clear-right">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
 		</div>
 	</>
 }
