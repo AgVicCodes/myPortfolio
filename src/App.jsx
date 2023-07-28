6f53db5b9c8a45bedbbd0cba91661306ad6d3591
AgVicCodes <victoragbeleye2@gmail.com>
2023-07-28 02:32:57 +0100

    Edited JSX

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function Myapp() {
 
 		<div className="Parent ">	
 			<div className="grid px-2 grid-cols-4 mx-auto">
-				<div className='m-2 h-32 bg-red-200  rounded-lg shadow-md'><h1 className='text-white bold'>Element 1</h1></div>
+				<div className='m-2 h-32 bg-red-200 hover:bg-red-900 ease-in-out duration-300 rounded-lg shadow-md'><h1 className='bg-slate-700 bold'>Element 1</h1></div>
 				<div className='m-2 h-32 bg-red-300 rounded-lg shadow-md'>Element 2</div>
 				<div className='m-2 h-32 bg-red-400 rounded-lg shadow-md'>Element 3</div>
 				<div className='m-2 h-32 bg-red-500 rounded-lg shadow-md'>Element 4</div>
