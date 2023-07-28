f974d18546b46d2b861366efc0b0098e3ebf126f
AgVicCodes <victoragbeleye2@gmail.com>
2023-07-28 02:44:47 +0100

    Edited JS

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
 				<nav className="navbar">
 					<div className="flex uppercase justify-center bg-zinc-400">
 						<ul className='flex flex-rows mx-4 my-1 text-neutral-700'>
-							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Home</li>
+							<li className='text-sm m-4 text-red-800 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Home</li>
 							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Projects</li>
 							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>About</li>
 							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Contact</li>
