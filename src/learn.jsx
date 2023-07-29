b8a6fd6d8fd2d6b692a736a62a81b82288df488a
AgVicCodes <victoragbeleye2@gmail.com>
2023-07-29 02:00:43 +0100

    Edited JSX

--- src/learn.jsx
+++ src/learn.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Learn() {
 	return (
 		<>
-			<div className="h-screen Parent">	
+			<div className="Parent">	
 				<div className="grid px-2 grid-cols-4">
-					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
-					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+					<div className='m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+					<div className='m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
 					{/* <div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
 					<div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div> */}
 				</div>
