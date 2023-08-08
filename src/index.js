1e24d19075b9e88cad639cf22ba41c6be52fa3b5
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-09 00:09:25 +0100

    Edited  JS

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ root.render(
   </React.StrictMode>
 );
 
-sdugoijug9
-
 // import reportWebVitals from './reportWebVitals';
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
