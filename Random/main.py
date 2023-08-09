e9fe8b2558fcd7f6695577204afe69aff5a1de22
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-09 22:32:45 +0100

    Added new folder Random

---
+++ Random/main.py
@@ -0,0 +1,15 @@
+"""
+git init            = Initialising git
+git status          = Showing the changes made  
+git add -A, ., -u   = Adding a new or modified change prior to committing
+git commit -m       = Saving your changes
+git diff            = Checking what changed
+git log             = List of all commits made to the project.
+git push -u origin  = Updating your code to github 
+                      master/main 
+git clone           = Copying a repository to your local directory
+git pull            = Get changes that has been made to the other projects by other developers.
+git checkout -b     = Create new branch.
+'branch-name'
+git merge           = Merge branch with master. 
+Vim (ProjectName)   = Opens a terminal to edit or view a file
