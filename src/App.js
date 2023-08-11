9640e40849e685566ae2d3ab30401bff5871505d
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-11 02:20:27 +0100

    Testing css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p className="text-red-500">Adding Tailwind to React</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
-			</header>
-		</div>
-	);
+export default function myapp() {
+	return <h1>Welcome here!</h1>
 }
 
-export default App;
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p className="text-red-500">Adding Tailwind to React</p>
+// 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
+// 			</header>
+// 		</div>
+// 	);
+// }
+
+// export default App;
