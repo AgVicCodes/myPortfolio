9f701d3523818f6cbb06a4ec6236f92e1c56911f
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-18 23:54:04 +0100

    Edited Py

--- Random/main.py
+++ Random/main.py
@@ -7,7 +7,7 @@ git diff            = Checking what changed
 git log             = List of all commits made to the project.
 git push -u origin  = Updating your code to github 
                       master/main 
-git clone           = Copying a repository to your local directory
+git clone           = Copying a repositorykhdavk to your local directory
 git pull            = Get changes that has been made to the other projects by other developers.
 git checkout -b     = Create new branch.
 'branch-name'
