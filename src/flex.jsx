15fd6ab6d22e91e95a9d8bce116ba67f980256e6
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-20 00:44:37 +0100

    Created Flex

---
+++ src/flex.jsx
@@ -0,0 +1,17 @@
+
+
+
+<div className="Parent">	
+    <div className="flex px-2 flex-row justify-center">
+        <div className='w-32 m-2 h-32 bg-red-100 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-200 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-300 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-500 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-600 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-700 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
+        <div className='w-32 m-2 h-32 bg-red-950 rounded-lg shadow-md'></div>
+    </div>
+</div>
