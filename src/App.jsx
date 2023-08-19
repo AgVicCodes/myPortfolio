d770c9058f790873e9c009a664e7e690a9dba1d3
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-20 00:21:41 +0100

    Edited App

--- src/App.jsx
+++ src/App.jsx
@@ -54,15 +54,6 @@ function Nav() {
 					<div className='w-32 m-2 h-32 bg-red-950 rounded-lg shadow-md'></div>
 				</div>
 			</div> */}
-			
-			<div className="h-screen Parent">	
-				<div className="grid px-2 grid-cols-4">
-					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
-					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
-					{/* <div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div> */}
-				</div>
-			</div>
 
 			{/* Drawing shapes with CSS */}
 		</>
