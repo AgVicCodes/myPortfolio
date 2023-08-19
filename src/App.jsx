63df7cc4fbe6ec586609c3d82fa4e1f031875bd8
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-20 00:44:27 +0100

    Edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import './Todo.js';
-import './Header.js';
+import Header from './Header.js';
+import Learn from './learn.jsx';
 
 // function Mytext() {
 // 	return <h1>Welcome here!</h1>
@@ -15,45 +16,8 @@ import './Header.js';
 function Nav() {
 	return (
 		<>
-			{/* <header className="Parent header bg-neutral-400">
-				<nav className="flex sm:justify-center space-x-4">
-					{[
-						['Home', '/dashboard'],
-						['Team', '/team'],
-						['Projects', '/projects'],
-						['Reports', '/reports'],
-					].map(([title, url]) => (
-						<ul className='my-4'>
-							<li><a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a></li>
-						</ul>
-					))}
-				</nav>
-				<nav className="navbar">
-					<div className="flex uppercase justify-center bg-zinc-400">
-						<ul className='flex flex-rows mx-4 my-1 text-neutral-700'>
-							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Home</li>
-							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Projects</li>
-							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>About</li>
-							<li className='text-sm m-4 hover:text-gray-900 hover:font-bold ease-in-out duration-300'>Contact</li>
-						</ul>
-					</div>
-				</nav>
-			</header> */}
+			<Header></Header>
 			
-			{/* <div className="Parent">	
-				<div className="flex px-2 flex-row justify-center">
-					<div className='w-32 m-2 h-32 bg-red-100 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-200 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-300 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-500 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-600 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-700 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
-					<div className='w-32 m-2 h-32 bg-red-950 rounded-lg shadow-md'></div>
-				</div>
-			</div> */}
 
 			{/* Drawing shapes with CSS */}
 		</>
@@ -61,7 +25,10 @@ function Nav() {
 }
 
 export default function Myapp() {
-	return <Nav />
+	return <>
+		<Nav />
+		<Learn />
+	</>;
 
 	// return <>
 	// 	<div className="Container">
