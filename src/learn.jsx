68d6e4d8b6bdb52ee105726961c443ea00c3130b
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-20 00:22:03 +0100

    Created new component

---
+++ src/learn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function learn() {
+	return (
+		<>
+			<div className="h-screen Parent">	
+				<div className="grid px-2 grid-cols-4">
+					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+					<div className='w-auto m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+					{/* <div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+					<div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div> */}
+				</div>
+			</div>  
+		</>
+	)
+}
