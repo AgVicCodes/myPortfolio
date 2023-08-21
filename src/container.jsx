7b077314d67c102eb14423b35f6f3d4590247067
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-21 14:24:01 +0100

    Edited JSX

--- src/container.jsx
+++ src/container.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Container() {
 	return <>
-		
+		<div className="container cols-4 mx-auto">
+			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
+			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
+			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
+			<div className="bg-red-300 h-16 py-5 px-4 text-white">One</div>
+		</div>
 	</>
 }
