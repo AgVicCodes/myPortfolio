c8c72f07b1e720f9c5a674bd31402ab295c37e01
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-22 15:49:04 +0100

    Moved Item

---
+++ src/components/container.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+
+export default function Container() {
+	return <>
+		<div className="container">
+			<img src="./logo192.png" alt="React logo" className="w-52 float-right" />
+			<img src="./logo192.png" alt="React logo" className="w-64 float-left" />
+			<div className="px-4 clear-right">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+			<div className="px-4">
+				<p>
+					One of your dependencies, babel-preset-react-app, is importing the
+					"@babel/plugin-proposal-private-property-in-object" package without        
+					declaring it in its dependencies. This is currently working because        
+					"@babel/plugin-proposal-private-property-in-object" is already in your     
+					node_modules folder for unrelated reasons, but it may break at any time.
+				</p>
+			</div>
+		</div>
+	</>
+}
