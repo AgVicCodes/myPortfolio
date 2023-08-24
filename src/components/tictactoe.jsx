16a84c8305fa17e4f72046b0b482d90b8adaa334
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-24 18:31:47 +0100

    Edited JSX

---
+++ src/components/tictactoe.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import { useState } from "react";
+
+function Square(value) {
+    const[value, setValue] = useState(null);
+    
+    function handleClick() {
+        setValue = "X";
+    }
+    
+    return <button onClick={handleClick} className="square">{value}</button>  
+}
+
+export default function Board() {
+    return <>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    </>
+}
