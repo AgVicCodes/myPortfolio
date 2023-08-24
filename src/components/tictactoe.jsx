6099c74cbc6d92050199ab57b072fc77927f1b19
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-24 20:51:24 +0100

    Edited JSX

--- src/components/tictactoe.jsx
+++ src/components/tictactoe.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { useState } from "react";
 
+var myChar = "X";
+// var nextChar = "Y";
+
 function Square() {
     const[value, setValue] = useState(null);
-    
+
     function handleClick() {
-        setValue('X');
+        setValue(myChar);
     }
     
     return <button onClick={handleClick} className="square">{value}</button>  
 }
+
+function setChar() {
+    if (myChar === 'X') {
+        myChar = 'O';
+    } else if (myChar === 'O') {
+        myChar = 'X';
+    }
+}
+
+
 
 export default function Board() {
     return <>
@@ -29,6 +42,7 @@ export default function Board() {
         <Square />
     </div>
 
-    <button className="w-32 mt-4 h-16 shadow-sm bg-slate-200 rounded-md">Swap Letters</button>
+    <button onClick={setChar} className="w-32 mt-4 h-16 shadow-sm bg-slate-200 rounded-md">Swap Letters</button>
+    <button onClick={setChar} className="w-32 mt-4 h-16 shadow-sm bg-slate-200 rounded-md">Clear Board</button>
     </>
 }
