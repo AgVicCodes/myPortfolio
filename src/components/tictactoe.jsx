19fe2c86ff8b1e39e17b582447c0c729d224dfd9
AgVicCodes <victoragbeleye2@gmail.com>
2023-08-30 12:47:31 +0100

    Edited JSX

--- src/components/tictactoe.jsx
+++ src/components/tictactoe.jsx
@@ -43,7 +43,7 @@ export default function Board() {
     <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick()} />
     </div>
 
     </>
