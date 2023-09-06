cb0b8ca0692a5ba20ed0887eab42b446238fd2ac
AgVicCodes <victoragbeleye2@gmail.com>
2023-09-06 05:26:51 +0100

    Moved Items

---
+++ src/components/ReactLesson/flex.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Flex() {
+  return (
+    <div className="Parent ">	
+        <div className="flex m-4 px-2 flex-row justify-center">
+            <div className='w-32 m-2 h-32 bg-red-100 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-200 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-300 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-400 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-500 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-600 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-700 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-800 rounded-lg shadow-md'></div>
+            <div className='w-32 m-2 h-32 bg-red-950 rounded-lg shadow-md'></div>
+        </div>
+    </div>
+  )
+}
+
+
