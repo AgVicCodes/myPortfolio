5231a3fb5bc591cad8b5de5192d6c517687f19f6
AgVicCodes <victoragbeleye2@gmail.com>
2023-09-16 11:33:52 +0100

    Edited JSX

--- src/components/propExample.jsx
+++ src/components/propExample.jsx
@@ -1,67 +1,45 @@
 import React from 'react';
-// import logo from "./components/image.jpg";
 
-// Reading props inside child component
-function Button ({size, color, text="yellow", bdr}) {
-    return <>
-        <button width="100" 
-        style={{
-            color: text,
-            backgroundColor: color,
-            borderRadius: size,
-            border: bdr,
-        }}>
-            Click Me
-        </button>
-    </>
-}
-
-// Nested prop
-function Card ({children}) {
-    return <>
-        <h1>Hello, this is my:
-            {children}
-        </h1>
-    </>
-}
-
-// Passing prop to components
+// import car from "../images/m3.jpg";
+// import bmw from "../images/m4.jpg";
+// import beamer from "../images/m5.jpg";
 
 
-export function PropExample() {
-    return <>
-        <h1>Hello World!</h1>
-        <Button 
-            color={'#777777'}
-            size={5}
-            bdr={"2px solid #ffff00"}
-        />
-        <Button 
-            color={'#d76b7a'}
-            size={1}
-            bdr={"2px solid #ffff00"}
-        />
-        <Button 
-            color={'#352700'}
-            text={'white'}
-            size={10}
-            bdr={"2px solid #ffff00"}
-        />
-
-        <Card>
-    		{/* <img src={logo} alt="A car" /> */}
-        </Card>
-    </>
-}
-
-// // Curly JSX Lesson
-// const Victor = {
-//     name: "Agbeleye Victor",
-//     style: {
-//         backgroundImage: './logo192.png',
-//         color: 'pink'
-//     },
-//     level: "BSc Graduate",
+// // Passing prop to components
+// function Avatar({person, size = 500, radius = 30 }) {
+//     return <>
+//         <h1>{person.userName}</h1>
+//         <img src={person.url} alt="myCar" width={size} height={size} style={{ borderRadius: radius }}/>
+//     </>
 // }
 
-{/* <h1 style={Victor.style}>{Victor.name}</h1> */}
+// export default function Gallery() {
+//     return <>
+//         <Avatar
+//             className = "first" 
+//             person = {{
+//                 userName: "Victor's First car",
+//                 url: car
+//             }}
+//             size = {100}
+//             radius = {10}
+//         />
+//         <Avatar
+//             className = "first" 
+//             person = {{
+//                 userName: "Victor's Second car",
+//                 url: bmw
+//             }}
+//             size = {300}
+//         />
+//         <Avatar
+//             className = "first" 
+//             person = {{
+//                 userName: "Victor's Third car",
+//                 url: beamer
+//             }}
+//             radius = {50}
+//         />
+//     </>
+// }
+
