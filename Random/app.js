28c4b524ba1399044c2e1a3311430e5ce82b143e
AgVicCodes <victoragbeleye2@gmail.com>
2023-09-16 11:34:20 +0100

    Edited JS

--- Random/app.js
+++ Random/app.js
@@ -211,3 +211,68 @@ export function Garbage (text) {
 // export default App;
 
 }
+
+// Reading props inside child component
+/*function Button ({size, color, text="yellow", bdr}) {
+    return <>
+        <button width="100" 
+        style={{
+            color: text,
+            backgroundColor: color,
+            borderRadius: size,
+            border: bdr,
+        }}>
+            Click Me
+        </button>
+    </>
+}*/
+
+// Nested prop
+/*function Card ({children}) {
+    return <>
+        <h1>Hello, this is my:
+            <h6>{children}</h6>
+        </h1>
+    </>
+}*/
+
+/*export function PropExample() {
+    return <>
+    <h1>Hello World!</h1>
+        <Button 
+            color={'#777777'}
+            size={5}
+            bdr={"2px solid #ffff00"}
+        />
+        <Button 
+        color={'#d76b7a'}
+        size={1}
+        bdr={"2px solid #ffff00"}
+        />
+        <Button 
+                    color={'#352700'}
+            text={'white'}
+            size={10}
+            bdr={"2px solid #ffff00"}
+        />
+
+        <Card>
+            Text
+        </Card>
+
+        <img src={logo} alt="A car" />
+        <h1 style={Victor.style}>{Victor.name}</h1>
+
+    </>
+}*/
+
+
+// // Curly JSX Lesson
+// const Victor = {
+//     name: "Agbeleye Victor",
+//     style: {
+//         backgroundImage: './logo192.png',
+//         color: 'pink'
+//     },
+//     level: "BSc Graduate",
+// }
