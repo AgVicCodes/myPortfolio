d0a49230364d8a94ad6ec6d933988026668b384e
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-01 01:36:16 +0100

    Edited JSX

---
+++ src/components/list.jsx
@@ -0,0 +1,60 @@
+const people = [
+    {
+        id: 1,
+        init: "Mary Slessor",
+        profession: "Chemist"
+    },
+    {
+        id: 2,
+        init: "Isaac Newton",
+        profession: "Physicist"
+    },
+    {
+        id: 3,
+        init: "Albert Einstein",
+        profession: "Physicist"
+    },
+    {
+        id: 4,
+        init: "Pythagoras",
+        profession: "Mathematician"
+    }
+];
+
+
+export default function List() {
+    const listItems = people.map(person => <li>
+        {person.init}: {person.profession}
+    </li>);
+    return <>
+        <ul>
+            {listItems}
+        </ul>
+    </>;
+}
+
+
+
+
+
+
+
+
+
+
+/*const people = [
+    "Mary Slessor: Chemist",
+    "Isaac Newton: Physicist",
+    "Albert Einstein: Physicist",
+    "Pythagoras: Mathematician"
+];
+
+
+export default function List() {
+    const listItems = people.map(person => <li>{person}</li>)
+    return <>
+        <ul>
+            {listItems}
+        </ul>
+    </>;
+}*/
