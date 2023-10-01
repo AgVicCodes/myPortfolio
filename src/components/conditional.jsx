ca143d11a5086a364a5aca559b137a211c999b5c
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-01 01:36:12 +0100

    Edited JSX

--- src/components/conditional.jsx
+++ src/components/conditional.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
+function Drink({ bevName }) {
+	return (
+		<section>
+			<h1>{bevName}</h1>
+			<table>
+				<tr>
+					<td>Part of plant</td>
+					<td>{bevName === 'tea' ? 'leaf' : 'bean'}</td>
+				</tr>
+				<tr>
+					<td>Caffeine content</td>
+					<td>{bevName === 'tea' ? '15 - 70 mg/cup' : '80 - 185 mg/cup'}</td>
+				</tr>
+				<tr>
+					<td>Age</td>
+					<td>{bevName === 'tea' ? '4,000+ years' : '1,000+ years'}</td>
+				</tr>
+			</table>
+		</section>
+	);
+}
 
-function Item({ name, isPacked }) {
-    return (
-      <li className="item">
-        {isPacked ? name + ' ✔' : name + ' ✖' }
-      </li>
-    );
-  }
-  
-  export default function PackingList() {
-    return (
-      <section>
-        <h1>Sally Ride's Packing List</h1>
-        <ul>
-          <Item 
-            isPacked={true} 
-            name="Space suit" 
-          />
-          <Item 
-            isPacked={true} 
-            name="Helmet with a golden leaf" 
-          />
-          <Item 
-            isPacked={false} 
-            name="Photo of Tam" 
-          />
-        </ul>
-      </section>
-    );
-  }
+export default function DrinkList() {
+	return (
+		<div>
+			<Drink bevName="tea" />
+			<Drink bevName="coffee" />
+		</div>
+	);
+}
   
