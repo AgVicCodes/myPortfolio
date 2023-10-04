f436fc4762acf4e05baa786586368607ae224ee6
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-04 17:59:16 +0100

    Edited JSX

---
+++ src/components/state.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function buttonState () {
+    const currentState = useState[null];
+}
+
+export default function State() {
+    return (<>
+        <div>state</div>
+    </>
+    );
+}
