8e7d33e046684fa42af28ac69edfa9b275a32666
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-11 06:45:15 +0100

    Edited JSX

--- src/components/galary.jsx
+++ src/components/galary.jsx
@@ -3,44 +3,85 @@ import { sculptureList } from './data.js';
 
 export default function Gallery() {
     const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
 
-    function handleNext() {
-        if (index >= sculptureList.length - 1) {
-            setIndex(0);
-        } else {
-            setIndex(index + 1);
-        }
-        // setIndex(index + 1);
+    function handleNextClick() {
+        setIndex(index + 1);
     }
 
-    function handlePrev() {
-        if (index >= 1) {
-            setIndex(index - 1);
-        } else {
-            setIndex(sculptureList.length - 1);
-        }
-            // setIndex(index - 1);
+    function handleMoreClick() {
+        setShowMore(!showMore);
     }
 
     let sculpture = sculptureList[index];
     return (
         <>
-            <button className="bg-green-600 p-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handleNext}>Next</button>
-            <h2><i>{sculpture.name} </i> by {sculpture.artist}</h2>
+            <button className="bg-green-600 p-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handleNextClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i> 
+                by {sculpture.artist}
+            </h2>
             <h3>  
                 ({index + 1} of {sculptureList.length})
             </h3>
+            <button className="bg-green-600 p-2 mb-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
             <img 
-                src={sculpture.url} alt={sculpture.alt} />
-            <p>{sculpture.description}</p>
-            <button className="bg-green-600 p-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handlePrev}>Previous</button>
+                src={sculpture.url} 
+                alt={sculpture.alt}
+            />
         </>
     );
 }
 
 
+{/* <button onClick={handleNext}>Next</button> */}
 
 
+
+// import { useState } from 'react';
+// import { sculptureList } from './data.js';
+
+// export default function Gallery() {
+//     const [index, setIndex] = useState(0);
+
+//     function handleNext() {
+//         if (index >= sculptureList.length - 1) {
+//             setIndex(0);
+//         } else {
+//             setIndex(index + 1);
+//         }
+//         // setIndex(index + 1);
+//     }
+
+//     function handlePrev() {
+//         if (index >= 1) {
+//             setIndex(index - 1);
+//         } else {
+//             setIndex(sculptureList.length - 1);
+//         }
+//             // setIndex(index - 1);
+//     }
+
+//     let sculpture = sculptureList[index];
+//     return (
+//         <>
+//             <button className="bg-green-600 p-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handleNext}>Next</button>
+//             <h2><i>{sculpture.name} </i> by {sculpture.artist}</h2>
+//             <h3>  
+//                 ({index + 1} of {sculptureList.length})
+//             </h3>
+//             <img 
+//                 src={sculpture.url} alt={sculpture.alt} />
+//             <p>{sculpture.description}</p>
+//             <button className="bg-green-600 p-2 hover:bg-green-800 border-2 border-green-900 rounded-md" onClick={handlePrev}>Previous</button>
+//         </>
+//     );
+// }
 
 
 
