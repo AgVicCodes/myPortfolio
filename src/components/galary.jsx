fd3dc3abff3a506c66cab0a46ce254df10b5c94a
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-12 04:34:53 +0100

    Omo

--- src/components/galary.jsx
+++ src/components/galary.jsx
@@ -1,3 +1,103 @@
+// import { getFinalState } from './processQueue.js';
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => 'n => n+1';
+
+export function getFinalState(baseState, queue) {
+    let finalState = baseState;
+  
+    for (let update of queue) {
+      if (typeof update === 'function') {
+        // Apply the updater function.
+        finalState = update(finalState);
+      } else {
+        // Replace the next state.
+        finalState = update;
+      }
+    }
+  
+    return finalState;
+  }
+  
+
+// export function getFinalState(baseState, queue) {
+//     let finalState = baseState;
+  
+//     // TODO: do something with the queue...
+  
+//     return finalState;
+// }  
+
+export default function App() {
+    return (
+        <>
+            <TestCase
+                baseState={0}
+                queue={[1, 1, 1]}
+                expected={1}
+            />
+            <hr />
+            <TestCase
+                baseState={0}
+                queue={[
+                increment,
+                increment,
+                increment
+                ]}
+                expected={3}
+            />
+            <hr />
+            <TestCase
+                baseState={0}
+                queue={[
+                5,
+                increment,
+                ]}
+                expected={6}
+            />
+            <hr />
+            <TestCase
+                baseState={0}
+                queue={[
+                5,
+                increment,
+                42,
+                ]}
+                expected={42}
+            />
+        </>
+    );
+}
+
+function TestCase({
+    baseState,
+    queue,
+    expected
+    }) {
+
+    const actual = getFinalState(baseState, queue);
+    
+    return (
+        <>
+            <p>Base state: <b>{baseState}</b></p>
+            <p>Queue: <b>[{queue.join(', ')}]</b></p>
+            <p>Expected result: <b>{expected}</b></p>
+            <p style={{
+                color: actual === expected ?
+                'green' :
+                'red'
+            }}>
+                Your result: <b>{actual}</b>
+                {' '}
+                ({actual === expected ?
+                'correct' :
+                'wrong'
+                })
+            </p>
+        </>
+    );
+}
 
 
 
@@ -5,12 +105,77 @@
 
 
 
+// import { useState } from 'react';
+
+// export default function RequestTracker() {
+//     const [pending, setPending] = useState(0);
+//     const [completed, setCompleted] = useState(0);
+
+//     async function handleClick() {
+//         setPending(pending + 1);
+//         await delay(3000);
+//         setPending(p => p - 1);
+//         setCompleted(c => c + 1);
+//     }
+
+//     return (
+//         <>
+//             <h3>
+//                 Pending: {pending}
+//             </h3>
+//             <h3>
+//                 Completed: {completed}
+//             </h3>
+//             <button onClick={handleClick}>
+//                 Buy     
+//             </button>
+//         </>
+//     );
+// }
+
+// function delay(ms) {
+//     return new Promise(resolve => {
+//         setTimeout(resolve, ms);
+//     });
+// }
 
 
 
+// Queueing
+
+// import { useState } from 'react';
+
+// export default function Counter() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 5);
+//         setNumber(n => n + 1);
+//         setNumber(42);
+//       }}>Increase the number</button>
+//     </>
+//   )
+// }
 
 
+// import { useState } from 'react';
 
+// export default function Counter() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 5);
+//         setNumber(n => n + 1);
+//       }}>Increase the number</button>
+//     </>
+//   )
+// }
 
 
 
