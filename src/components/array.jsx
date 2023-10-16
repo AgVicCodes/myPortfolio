331b341e4e8f4b6f39c5f2d1e21f13b5cc2b46a9
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-16 01:39:48 +0100

    Edited JSX

--- src/components/array.jsx
+++ src/components/array.jsx
@@ -1,5 +1,17 @@
-export default function array() {
+import { useState } from "react";
+
+export default function Array() {
+    
+    let nextId = 0;
+
+    const [name, setName] = useState("Victor");
+    const [artist, setArtist] = useState([]);
+    
     return (
-        <div>array</div>
+        <div>
+            <h1>Artists of the Century</h1>
+            <input type="text" name={name} />
+            <button className="bg-green-700 border-2 border-green-900 rounded-md text-gray-100 p-2"></button>
+        </div>
     )
 }
