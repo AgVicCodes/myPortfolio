e0f9725b333c258908aa497c15f6f0b822cc229f
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-16 01:57:35 +0100

    Edited JSX

--- src/components/array.jsx
+++ src/components/array.jsx
@@ -1,17 +1,77 @@
 import { useState } from "react";
 
-export default function Array() {
-    
-    let nextId = 0;
 
-    const [name, setName] = useState("Victor");
-    const [artist, setArtist] = useState([]);
+
+
+
+
+
+// // The right way
+
+// export default function Array() {
     
-    return (
-        <div>
-            <h1>Artists of the Century</h1>
-            <input type="text" name={name} />
-            <button className="bg-green-700 border-2 border-green-900 rounded-md text-gray-100 p-2"></button>
-        </div>
-    )
-}
+//     let nextId = 0;
+
+//     const [name, setName] = useState("Victor");
+//     const [artist, setArtist] = useState([]);
+    
+//     return (
+//         <div>
+//             <h1>Artists of the Century</h1>
+//             <input type="text" className="border-2 border-green-900 rounded-md text-gray-800 p-2 mr-4" value={name} 
+//             onChange={(e) => setName(e.target.value)}/>
+//             <button className="bg-green-700 border-2 border-green-900 rounded-md text-gray-100 p-2" onClick={() => {
+//                 setArtist([
+//                     ...artist,
+//                     {
+//                         id: nextId,
+//                         name: name
+//                     }
+//                 ])
+//             }}>
+//                 Add
+//             </button>
+//             <ul>
+//                 {artist.map(artist => (
+//                     <li key={artist.id}>
+//                         {artist.name}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+// // Mutating the button Function
+
+// export default function Array() {
+    
+//     let nextId = 0;
+
+//     const [name, setName] = useState("Victor");
+//     const [artist, setArtist] = useState([]);
+    
+//     return (
+//         <div>
+//             <h1>Artists of the Century</h1>
+//             <input type="text" className="border-2 border-green-900 rounded-md text-gray-800 p-2 mr-4" value={name} 
+//             onChange={(e) => setName(e.target.value)}/>
+//             <button className="bg-green-700 border-2 border-green-900 rounded-md text-gray-100 p-2" onClick={() => {
+//                 artist.push({
+//                     id: nextId++,
+//                     name: name
+//                 });
+//             }}>
+//                 Add
+//             </button>
+//             <ul>
+//                 {artist.map(artist => (
+//                     <li key={artist.id}>
+//                         {artist.name}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
