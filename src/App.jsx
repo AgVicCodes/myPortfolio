9086832d7061267f8dc8b8969e6ae7114b97d394
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-17 05:22:22 +0100

    Moved Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
-import App from "./components/array";
 
 export default function Myapp() {
 	return (<>
-		<App />
-		{/* <State /> */}
-		{/* <Checker /> */}
-	</>
+		
+		</>
 	);
 }
