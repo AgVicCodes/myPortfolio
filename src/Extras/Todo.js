8303e9ef8ef879b7efd59dc3cda70d8a2dca4414
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-17 06:08:24 +0100

    Moved Items

---
+++ src/Extras/Todo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Todo() {
+	return (
+		<div>Todo</div>
+	)
+}
+
+
+// import React from 'react'
+
+// export default function header() {
+// 	return (
+// 		<>
+// 			<header>
+// 				<nav>
+// 					<ul>
+// 						<li>Home</li>
+// 						<li>Projects</li>
+// 						<li>About</li>
+// 						<li>Contact</li>
+// 					</ul>
+// 				</nav>
+// 			</header>
+// 		</>
+// 	)
+// }
+
+// composer install laravel/laravel UI
+// composer create-laravel-app my-app: ^8.0
+// composer create-project laravel/laravel:^8.0 my-app
