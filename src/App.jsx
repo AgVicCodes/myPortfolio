b5dd9280f4bc2418fb53e59cc1ca2b25356c0761
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-20 09:35:24 +0100

    Moved files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import "./App.css";
 
 export default function Myapp() {
 	return (<>
-		
+			<header>
+				<nav classname="bg-green-500">
+					<ul>
+						<li>Projects</li>
+						<li>Contact</li>
+						<li>About</li>
+					</ul>
+				</nav>
+			</header>
 		</>
 	);
 }
