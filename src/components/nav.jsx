5501a9cf362f9f0bb472a8361bb41adf29e74f13
AgVicCodes <victoragbeleye2@gmail.com>
2023-10-22 07:45:36 +0100

    Edited JSX

---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+export default function Navbar () {
+    return <>
+        <div className="main-nav">
+            <header className="">
+                <nav>
+                    <ul className="flex float-right">
+                        <li className="mr-4"></li>
+                        <li className="mr-4"></li>
+                        <li className="mr-4"></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    </>
+}
