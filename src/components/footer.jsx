6ccbe2b539c72b030ec589573696b17cc588db53
AgVictorCodes <victoragbeleye2@gmail.com>
2024-09-19 12:30:54 +0100

    Indentation

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,8 @@
 <footer className="flex justify-between h-24 px-4 md:px-16 lg:px-32">
-    <div className="text-xs md:text-lg lg:text-xl my-auto text-neutral-300">
-        Copyright © 2024 AgVictorCodes. 
-    </div>
-    <div className="text-xs md:text-lg lg:text-xl my-auto text-neutral-300">
-        Designed by AgVictorCodes♥
-    </div>
-</footer>
+        <div className="text-xs md:text-lg lg:text-xl my-auto text-neutral-300">
+            Copyright © 2024 AgVictorCodes. 
+        </div>
+        <div className="text-xs md:text-lg lg:text-xl my-auto text-neutral-300">
+            Designed by AgVictorCodes♥
+        </div>
+    </footer>
